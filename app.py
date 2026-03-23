80ff42ee655225434a1218667f0cd6ef87ab315a
sahana-055 <sahanac055@gmail.com>
2026-03-23 21:55:28 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,25 @@
+from fastapi import FastAPI
+import google.generativeai as genai
+
+app = FastAPI()
+
+# Add your Gemini API key
+genai.configure(api_key="AIzaSyC3LmBXbsX9J1AO9XJjBl3CzSYte-mSnXg")
+
+model = genai.GenerativeModel("gemini-1.5-flash")
+
+@app.get("/")
+def home():
+    return {"message": "AI Agent is running!"}
+
+@app.post("/summarize")
+def summarize(text: str):
+    prompt = f"Summarize this text in simple words:\n{text}"
+    
+    response = model.generate_content(prompt)
+    
+    return {"summary": response.text}
+if __name__ == "__main__":
+    import uvicorn
+    port = int(os.environ.get("PORT", 10000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
